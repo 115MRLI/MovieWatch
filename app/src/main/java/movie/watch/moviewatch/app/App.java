137013812a6a1bfebc03db0ff970a4da805f231d
115MRLI <1159642490@qq.com>
2018-06-27 16:43:44 +0800

    加入环信sdk

--- app/src/main/java/movie/watch/moviewatch/app/App.java
+++ app/src/main/java/movie/watch/moviewatch/app/App.java
@@ -1,11 +1,13 @@
 package movie.watch.moviewatch.app;
 
+import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
-import android.graphics.Typeface;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.SupportActivity;
-import android.util.Log;
+
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMOptions;
 
 import org.xutils.DbManager;
 import org.xutils.x;
@@ -14,19 +16,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
-import java.util.logging.Logger;
-
-import cn.bmob.newim.BmobIM;
-import cn.bmob.push.BmobPush;
-import cn.bmob.v3.Bmob;
-import cn.bmob.v3.BmobInstallation;
-import cn.bmob.v3.BmobInstallationManager;
-import cn.bmob.v3.InstallationListener;
-import cn.bmob.v3.exception.BmobException;
-import movie.watch.moviewatch.utils.BmobMessageHandler;
-
-import static cn.bmob.newim.util.Utils.getMyProcessName;
 
 
 public class App extends Application {
@@ -34,7 +26,8 @@ public class App extends Application {
     private Set<FragmentActivity> allActivities;
     public static Context mContext;
     private static DbManager.DaoConfig daoConfig;
-
+    // 记录是否已经初始化
+    private boolean isInit = false;
     public DbManager.DaoConfig getDaoConfig() {
         return daoConfig;
     }
@@ -93,36 +86,102 @@ public class App extends Application {
         instance = this;
         mContext = getApplicationContext();
         initializedDatabase();
-        initBmob();
+        // 初始化环信SDK
+        initEasemob();
     }
 
     /**
-     * 初始化Bmob信息
+     * 初始化环信SDK
      */
-    private void initBmob() {
-        //TODO 集成：1.4、初始化数据服务SDK、初始化设备信息并启动推送服务
-        // 初始化BmobSDK
-        Bmob.initialize(this, "af8174f3afdde0f3ff885d12fb91a6e4");
-        // 使用推送服务时的初始化操作
-        BmobInstallationManager.getInstance().initialize(new InstallationListener<BmobInstallation>() {
-            @Override
-            public void done(BmobInstallation bmobInstallation, BmobException e) {
-                if (e == null) {
-                    Log.i("bmob", bmobInstallation.getObjectId() + "-" + bmobInstallation.getInstallationId());
-                } else {
-                    Log.e("bmob", e.getMessage());
-                }
-            }
-        });
-        // 启动推送服务
-        BmobPush.startWork(this);
-
-        //TODO 集成：1.8、初始化IM SDK，并注册消息接收器
-        if (getApplicationInfo().packageName.equals(getMyProcessName())) {
-            BmobIM.init(this);
-            BmobIM.registerDefaultMessageHandler(new BmobMessageHandler());
+    private void initEasemob() {
+        // 获取当前进程 id 并取得进程名
+        int pid = android.os.Process.myPid();
+        String processAppName = getAppName(pid);
+        /**
+         * 如果app启用了远程的service，此application:onCreate会被调用2次
+         * 为了防止环信SDK被初始化2次，加此判断会保证SDK被初始化1次
+         * 默认的app会在以包名为默认的process name下运行，如果查到的process name不是app的process name就立即返回
+         */
+        if (processAppName == null || !processAppName.equalsIgnoreCase(mContext.getPackageName())) {
+            // 则此application的onCreate 是被service 调用的，直接返回
+            return;
         }
+        if (isInit) {
+            return;
+        }
+
+        // 调用初始化方法初始化sdk
+        EMClient.getInstance().init(mContext, initOptions());
+        // 设置开启debug模式
+        EMClient.getInstance().setDebugMode(true);
+
+        // 设置初始化已经完成
+        isInit = true;
     }
+
+    /**
+     * SDK初始化的一些配置
+     * 关于 EMOptions 可以参考官方的 API 文档
+     * http://www.easemob.com/apidoc/android/chat3.0/classcom_1_1hyphenate_1_1chat_1_1_e_m_options.html
+     */
+    private EMOptions initOptions() {
+
+        EMOptions options = new EMOptions();
+        // 设置Appkey，如果配置文件已经配置，这里可以不用设置
+        // options.setAppKey("lzan13#hxsdkdemo");
+        // 设置自动登录
+        options.setAutoLogin(true);
+        // 设置是否需要发送已读回执
+        options.setRequireAck(true);
+        // 设置是否需要发送回执，
+        options.setRequireDeliveryAck(true);
+        // 设置是否根据服务器时间排序，默认是true
+        options.setSortMessageByServerTime(false);
+        // 收到好友申请是否自动同意，如果是自动同意就不会收到好友请求的回调，因为sdk会自动处理，默认为true
+        options.setAcceptInvitationAlways(false);
+        // 设置是否自动接收加群邀请，如果设置了当收到群邀请会自动同意加入
+        options.setAutoAcceptGroupInvitation(false);
+        // 设置（主动或被动）退出群组时，是否删除群聊聊天记录
+        options.setDeleteMessagesAsExitGroup(false);
+        // 设置是否允许聊天室的Owner 离开并删除聊天室的会话
+        options.allowChatroomOwnerLeave(true);
+        // 设置google GCM推送id，国内可以不用设置
+        // options.setGCMNumber(MLConstants.ML_GCM_NUMBER);
+        // 设置集成小米推送的appid和appkey
+        // options.setMipushConfig(MLConstants.ML_MI_APP_ID, MLConstants.ML_MI_APP_KEY);
+
+        return options;
+    }
+
+    /**
+     * 根据Pid获取当前进程的名字，一般就是当前app的包名
+     *
+     * @param pid 进程的id
+     * @return 返回进程的名字
+     */
+    private String getAppName(int pid) {
+        String processName = null;
+        ActivityManager activityManager = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
+        List list = activityManager.getRunningAppProcesses();
+        Iterator i = list.iterator();
+        while (i.hasNext()) {
+            ActivityManager.RunningAppProcessInfo info =
+                    (ActivityManager.RunningAppProcessInfo) (i.next());
+            try {
+                if (info.pid == pid) {
+                    // 根据进程的信息获取当前进程的名字
+                    processName = info.processName;
+                    // 返回当前进程名
+                    return processName;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        // 没有匹配的项，返回为null
+        return null;
+    }
+
 
     /**
      * 获取当前运行的进程名
