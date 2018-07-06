e48b1e8ddf82c96fe16e286b846ea1d62b169a0c
115MRLI <1159642490@qq.com>
2018-07-06 10:01:39 +0800

    欢迎页面设计

--- app/src/main/java/movie/watch/moviewatch/view/activity/MainActivity.java
+++ app/src/main/java/movie/watch/moviewatch/view/activity/MainActivity.java
@@ -6,10 +6,13 @@ import butterknife.BindView;
 import movie.watch.loading.ColoringLoadingView;
 import movie.watch.moviewatch.R;
 import movie.watch.moviewatch.base.BaseActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends BaseActivity {
     @BindView(R.id.main_loading)
     ColoringLoadingView loadingView;
+    @BindView(R.id.wel_bc)
+    ImageView  weiBc;
 
     @Override
     protected int getLayout() {
@@ -19,7 +22,7 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView() {
         super.initView();
-        loadingView.setCharacter(ColoringLoadingView.Character.AK);
+        loadingView.setCharacter(ColoringLoadingView.Character.CAT);
         loadingView.setColoringColor(0xFFFF1744);
         loadingView.setVisibility(View.VISIBLE);
         loadingView.startDrawAnimation();
